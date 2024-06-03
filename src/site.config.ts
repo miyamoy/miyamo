b386d269712567f211ac33a48f2df056e7b59483
miyamoy <miyamo765@gmail.com>
2024-06-03 18:29:17 +0900

    add site.config.ts

---
+++ src/site.config.ts
@@ -0,0 +1,56 @@
+import type { SiteConfig } from "@/types";
+import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
+
+export const siteConfig: SiteConfig = {
+    author: "Yasuaka Miyamoto",
+    date: {
+        locale: "ja-JP",
+        options: {
+            day: "numeric",
+            month: "short",
+            year: "numeric"
+        },
+    },
+    description: "miyamo",
+    lang: "ja-JP",
+    ogLocale: "ja_JP",
+    sortPostsByUpdatedDate: true,
+    title: "miyamo",
+};
+
+export const menuLinks: { path: string, title: string }[] = [
+    {
+        path: "/",
+        title: "Home",
+    },
+    {
+        path: "/about/",
+        title: "About",
+    },
+    {
+        path: "posts",
+        title: "Blog",
+    },
+];
+
+export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+    styleOverrides: {
+        borderRadius: "4px",
+		codeFontFamily:
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+		codeFontSize: "0.875rem",
+		codeLineHeight: "1.7142857rem",
+		codePaddingInline: "1rem",
+		uiLineHeight: "inherit",
+    },
+    themeCssSelector(theme, { styleVariants }) {
+        if (styleVariants.length >= 2) {
+            const baseTheme = styleVariants[0]?.theme;
+            const altTheme  = styleVariants.find((v) => v.theme.type != baseTheme?.type)?.theme;
+            if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`; 
+        }
+        return `[data-theme='${theme.type}']`;
+    },
+    themes: ["dracula", "github-light"],
+	useThemedScrollbars: false,
+}
